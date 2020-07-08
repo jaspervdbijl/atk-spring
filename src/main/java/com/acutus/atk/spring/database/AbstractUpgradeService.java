2b2be7bc9d21044f71fb7f3bdade2f5e25e58798
Jasper van der Bijl <jasper@vanderbijl.co>
2020-07-08 16:06:00 +0200

    https://cellstop.atlassian.net/browse/VD-9

---
+++ src/main/java/com/acutus/atk/spring/database/AbstractUpgradeService.java
@@ -0,0 +1,45 @@
+package com.acutus.atk.spring.database;
+
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import static com.acutus.atk.db.sql.SQLHelper.run;
+
+@Component
+@Slf4j
+public abstract class AbstractUpgradeService {
+
+    @Value("${database.liquibase.enabled:false}")
+    private boolean liquibaseEnabled;
+
+    @Autowired
+    DataSource dataSource;
+
+    public abstract void maintainPackages();
+
+
+    protected  void runLiquibase(String file) {
+        if (liquibaseEnabled) {
+            log.info("Start runLiquibase master");
+            run(dataSource, connection -> {
+                Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+                Liquibase liquibase = new Liquibase(file, new ClassLoaderResourceAccessor(), database);
+                liquibase.update((Contexts) null);
+            });
+            log.info("Finished runLiquibase");
+        }
+    }
+
+}
