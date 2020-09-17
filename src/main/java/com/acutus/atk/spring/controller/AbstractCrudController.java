26dcc6fd65a4ac947b5ca1d69e67e84ddd8b0b89
Jasper van der Bijl <jasper@vanderbijl.co>
2020-09-17 12:47:56 +0200

    https://cellstop.atlassian.net/browse/VD-52

--- src/main/java/com/acutus/atk/spring/controller/AbstractCrudController.java
+++ src/main/java/com/acutus/atk/spring/controller/AbstractCrudController.java
@@ -1,50 +1,45 @@
 package com.acutus.atk.spring.controller;
 
-import com.acutus.atk.db.AbstractAtkEntity;
-import com.acutus.atk.db.AtkEnFields;
-import com.acutus.atk.db.Persist;
-import com.acutus.atk.db.Query;
+import com.acutus.atk.db.*;
 import com.acutus.atk.db.sql.Filter;
 import com.acutus.atk.reflection.Reflect;
 import com.acutus.atk.reflection.ReflectMethods;
 import com.acutus.atk.util.Assert;
+import com.acutus.atk.util.call.CallOne;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.lang.Collections;
 import lombok.SneakyThrows;
+import org.springframework.beans.SimpleTypeConverter;
+import org.springframework.beans.TypeConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.acutus.atk.db.sql.Filter.Type.AND;
 
 public interface AbstractCrudController<T extends AbstractAtkEntity> {
 
     public DataSource getDataSource();
 
+    static ObjectMapper mapper = new ObjectMapper();
+    static SimpleTypeConverter typeConverter = new SimpleTypeConverter();
+
     /**
      * get the generic type - T
+     *
      * @return
      */
     public default Class<T> getType() {
-        return (Class<T>) ((ParameterizedType) getClass()
-                .getGenericSuperclass()).getActualTypeArguments()[0];
-    }
-
-    @SneakyThrows
-    default Method getMethod(String name) {
-        ReflectMethods methods = Reflect.getMethods(getType()).filter(name).filterParams();
-        Assert.isTrue(methods.size() == 1,"Expected a %s method in %s ",name, getType().getName());
-        return methods.get(0);
-    }
-
-    @SneakyThrows
-    default Query getQuery(T entity) {
-        return (Query) getMethod("query").invoke(entity);
-    }
-
-    @SneakyThrows
-    default Persist getPersist(T entity) {
-        return (Persist) getMethod("persist").invoke(entity);
+        return (Class<T>) ((ParameterizedType) getClass().getGenericInterfaces()[0])
+                .getActualTypeArguments()[0];
     }
 
     @SneakyThrows
@@ -54,31 +49,55 @@ public interface AbstractCrudController<T extends AbstractAtkEntity> {
 
     @SneakyThrows
     @RequestMapping(method = RequestMethod.GET)
-    public default List<T> getAll() {
-        return getQuery(getInstance()).getAll(getDataSource(),new Filter());
+    public default List getAll() {
+        List values = new ArrayList();
+        getInstance().query().getAll(getDataSource(), new Filter(), (CallOne<AbstractAtkEntity>) o -> values.add(o.toBase()), 100);
+        return values;
     }
 
-    @RequestMapping(method = RequestMethod.GET,path = "/{id}")
-    public default @ResponseBody T getById(@PathVariable("id") String id) {
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public default @ResponseBody
+    Object getById(@PathVariable("id") String id) {
         T instance = getInstance();
         AtkEnFields ids = instance.getEnFields().getIds();
-        Assert.isTrue(ids.size() == 1,"Expected exactly one id field for " + getType());
+        Assert.isTrue(ids.size() == 1, "Expected exactly one id field for " + getType());
 
-        ids.get(0).set(id);
+        ids.get(0).set(typeConverter.convertIfNecessary(id, ids.get(0).getField().getType()));
 
-        Optional<T> entity = (Optional<T>) getQuery(instance).get(getDataSource());
-        Assert.isTrue(entity.isPresent(),() -> new RuntimeException("Entity not found"));
-        return entity.get();
+        Optional<T> entity = (Optional<T>) instance.query().get(getDataSource());
+        Assert.isTrue(entity.isPresent(), () -> new RuntimeException("Entity not found"));
+        return entity.get().toBase();
+    }
+
+    @SneakyThrows
+    @RequestMapping(method = RequestMethod.GET, path = "/queryBySet")
+    public default @ResponseBody
+    List<T> queryBySet(@RequestBody String entity
+            , @RequestParam(required = false) String orderBy, @RequestParam(required = false) Boolean orderByAsc) {
+        T instance = mapper.readValue(entity, getType());
+        Query query = instance.query();
+        if (orderBy != null) {
+            orderByAsc = orderByAsc == null ? false : orderByAsc;
+            query.setOrderBy(orderByAsc ? Query.OrderBy.ASC : Query.OrderBy.DESC, instance.getEnFields().getByColName(orderBy).get().getField());
+        }
+        List values = new ArrayList();
+        query.getAll(getDataSource(), new Filter(AND, instance.getEnFields().getSet()), (e) -> {
+            values.add(((AbstractAtkEntity)e).toBase());
+        }, 100);
+        return values;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public default void create(@RequestBody T entity) {
-        getPersist(getInstance()).insert(getDataSource());
+        getInstance().persist().insert(getDataSource());
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     public default void update(@RequestBody T entity) {
-        getPersist(getInstance()).update(getDataSource());
+        // TODO only certain fields may be updated
+        entity.getFields().stream().forEach(f -> ((AtkEnField)f).setSet(true));
+        entity.persist().update(getDataSource());
     }
+
 
 }
