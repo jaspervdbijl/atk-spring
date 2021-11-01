ed495ba191f0c9d67b7e6ebb0ab59fc52c721a49
Jasper van der Bijl <jasper@vanderbijl.co>
2021-11-01 12:25:39 +0200

    Added Gis Reverse Geo service to resolve all locations

--- src/main/java/com/acutus/atk/spring/services/auth/Security.java
+++ src/main/java/com/acutus/atk/spring/services/auth/Security.java
@@ -2,13 +2,13 @@ package com.acutus.atk.spring.services.auth;
 
 
 import com.acutus.atk.io.IOUtil;
+import com.acutus.atk.util.StringUtils;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
@@ -33,6 +33,37 @@ public class Security {
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    @SneakyThrows
+    private static Signature getSignature(KeyPair keyPair) {
+        Signature sig = Signature.getInstance("SHA1WithRSA");
+        sig.initSign(keyPair.getPrivate());
+        return sig;
+    }
+
+    @SneakyThrows
+    public static byte[] signData(KeyPair keyPair, byte data[]) {
+        Signature sig = getSignature(keyPair);
+        sig.update(data);
+        return sig.sign();
+    }
+
+    @SneakyThrows
+    public static void verifySignedData(KeyPair keyPair, byte data[], byte signatureBytes[]) {
+        Signature sig = getSignature(keyPair);
+        sig.initVerify(keyPair.getPublic());
+        sig.update(data);
+
+        sig.verify(signatureBytes);
+    }
+
+    @SneakyThrows
+    public static String md5Hash(byte data[]) {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(data);
+        byte[] digest = md.digest();
+        return StringUtils.bytesToHex(digest).toUpperCase();
     }
 
 }
