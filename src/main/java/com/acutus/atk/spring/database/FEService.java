65d40593498fbe3aee458634ad552e5a2725bb5d
Jasper van der Bijl <jasper@vanderbijl.co>
2022-04-06 09:45:10 +0200

    Virtual Trips re generation

--- src/main/java/com/acutus/atk/spring/database/FEService.java
+++ src/main/java/com/acutus/atk/spring/database/FEService.java
@@ -4,10 +4,12 @@ import com.acutus.atk.db.AbstractAtkEntity;
 import com.acutus.atk.db.fe.FEHelper;
 import com.acutus.atk.db.processor.AtkEntity;
 import com.acutus.atk.spring.util.Reflections;
+import com.acutus.atk.util.Strings;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.acutus.atk.db.Persist.setPersistCallback;
@@ -21,17 +23,21 @@ public class FEService {
         setPersistCallback((connection, entity, isInsert) -> audit(connection, entity, isInsert));
     }
 
-    public static void maintainPackages(Connection connection, String... packages) {
-        FEHelper.maintainDataDefinition(connection, Arrays.asList(packages)
+    public static void maintainPackages(Connection connection, List<String> packages, Strings excludes) {
+        FEHelper.maintainDataDefinition(connection, packages
                 .stream()
                 .map(p -> new Reflections(p).getSubTypesOf(AbstractAtkEntity.class))
                 .flatMap(l -> l.stream())
-                .filter(a -> handle(() -> a.getConstructor().newInstance().getEntityType() == AtkEntity.Type.TABLE))
+                .filter(a -> excludes.startsWith(a.getPackageName()).isEmpty() && handle(() -> a.getConstructor().newInstance().getEntityType() == AtkEntity.Type.TABLE))
                 .collect(Collectors.toList()));
     }
 
     public static void maintainPackages(DataSource dataSource, String... packages) {
-        run(dataSource, c -> maintainPackages(c, packages));
+        run(dataSource, c -> maintainPackages(c, Arrays.asList(packages),new Strings()));
+    }
+
+    public static void maintainPackages(DataSource dataSource, List<String> packages, Strings excludes) {
+        run(dataSource, c -> maintainPackages(c, packages,excludes));
     }
 
     public static void maintainClasses(DataSource dataSource, Class... classes) {
