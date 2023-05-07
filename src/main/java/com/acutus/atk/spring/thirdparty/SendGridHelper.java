e36b410a39921f6adc12d0333e0f6ec99970b8f4
jaspervdbijl <jaspervdb@cellstop.org>
2023-05-07 13:22:32 +0200

    Nigeria New Units IGN Fix

--- src/main/java/com/acutus/atk/spring/thirdparty/SendGridHelper.java
+++ src/main/java/com/acutus/atk/spring/thirdparty/SendGridHelper.java
@@ -1,15 +1,13 @@
 package com.acutus.atk.spring.thirdparty;
 
 import com.acutus.atk.util.Assert;
+import com.acutus.atk.util.IOUtil;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Attachments;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-import com.sendgrid.helpers.mail.objects.Personalization;
+import com.sendgrid.helpers.mail.objects.*;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +15,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 @Component
@@ -81,6 +83,17 @@ public class SendGridHelper {
         return response;
     }
 
+    @SneakyThrows
+    public Attachments toAttachments(File csvFile, String filename) {
+        Attachments attachments = new Attachments();
+        attachments.setContent(Base64.getEncoder().encodeToString(IOUtil.readAvailable(new FileInputStream(csvFile))));
+        attachments.setType("text/csv");
+        attachments.setFilename(filename);
+        attachments.setDisposition("attachment");
+        attachments.setContentId("Reports");
+        return attachments;
+    }
+
     public Response sendEmailPlainTxt(String address, String subject, String txt, List<Attachments> attachments) {
         Personalization personalization = new Personalization();
         Arrays.stream(address.split("[,|;]")).forEach(a -> personalization.addTo(new Email(a)));
@@ -101,20 +114,28 @@ public class SendGridHelper {
         return response;
     }
 
-    public void postEmail(Boolean maySend, String templateId, List<Personalization> personalizationList) {
-        Assert.notNull(templateId, "Template Id must be supplied");
-        if (personalizationList.isEmpty() || !maySend) {
-            return;
-        }
+    public ResponseEntity<String> sendTemplate(String templateId, Integer asmGroupId, List<Personalization> personalizationList, List<Attachments> attachments) {
 
         Mail mail = new Mail();
         mail.setFrom(new Email(fromAddress));
         mail.setTemplateId(templateId);
+        ASM asm = new ASM();
+        asm.setGroupId(asmGroupId);
+        asm.setGroupsToDisplay(new int[]{asmGroupId});
+        mail.setASM(asm);
         mail.personalization = personalizationList;
-        try {
-            sendGridRestTemplate.postForEntity(sendGridUrl, new HttpEntity<>(mail, sendGridRestTemplateHeaders), String.class);
-        } catch (Exception exception) {
-            log.error(exception.getMessage(), exception);
+
+        if (!CollectionUtils.isEmpty(attachments)) {
+            attachments.stream().forEach(a -> mail.addAttachments(a));
         }
+
+        return sendGridRestTemplate.postForEntity(sendGridUrl, new HttpEntity<>(mail, sendGridRestTemplateHeaders), String.class);
+    }
+
+    public ResponseEntity<String> sendTemplate(String templateId, Integer asmGroupId, List<String> address, String subject, List<Attachments> attachments) {
+        Personalization personalization = new Personalization();
+        address.stream().forEach(a -> personalization.addTo(new Email(a)));
+        personalization.setSubject(subject);
+        return sendTemplate(templateId, asmGroupId,Arrays.asList(personalization),attachments);
     }
 }
