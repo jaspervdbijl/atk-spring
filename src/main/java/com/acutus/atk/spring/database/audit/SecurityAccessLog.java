5eb899daee99dd829d63adc8c46701925218714d
jaspervdbijl <jaspervdb@cellstop.org>
2024-05-29 13:19:42 +0200

    https://cellstop.atlassian.net/browse/VD-385

---
+++ src/main/java/com/acutus/atk/spring/database/audit/SecurityAccessLog.java
@@ -0,0 +1,22 @@
+package com.acutus.atk.spring.database.audit;
+
+import com.acutus.atk.db.annotations.UID;
+import com.acutus.atk.db.processor.AtkEntity;
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AtkEntity(columnNamingStrategy = AtkEntity.ColumnNamingStrategy.CAMEL_CASE_UNDERSCORE, enableAuditByUser = true, maintainEntity = true)
+@Table(name = "a_security_access_log")
+@Data
+public class SecurityAccessLog {
+    @Id
+    @GeneratedValue()
+    private Integer id;
+
+    private String action;
+    private String status;
+
+}
