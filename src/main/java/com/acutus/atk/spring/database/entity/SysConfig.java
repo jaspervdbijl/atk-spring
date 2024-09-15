c7daca21f41077631188000554f8a90c2ae4dcb0
jaspervdbijl <jaspervdbijl@gmail.com>
2024-09-15 10:42:35 +0200

    https://cellstop.atlassian.net/browse/VD-404

---
+++ src/main/java/com/acutus/atk/spring/database/entity/SysConfig.java
@@ -0,0 +1,26 @@
+package com.acutus.atk.spring.database.entity;
+
+import com.acutus.atk.db.processor.AtkEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AtkEntity(columnNamingStrategy = AtkEntity.ColumnNamingStrategy.CAMEL_CASE_UNDERSCORE)
+@Table(name = "sysconfig")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class SysConfig {
+
+    @Id @Column(length = 100)
+    private String name;
+
+    @Column(length = 4096)
+    String value;
+}
---
+++ src/main/java/com/acutus/atk/spring/util/properties/DbValueProcessor.java
@@ -0,0 +1,82 @@
+package com.acutus.atk.spring.util.properties;
+
+import com.acutus.atk.spring.database.entity.SysConfigEntity;
+import com.acutus.atk.util.Assert;
+import lombok.SneakyThrows;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import javax.sql.DataSource;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class DbValueProcessor implements BeanPostProcessor {
+
+    @Autowired
+    DataSource dataSource;
+
+    private Map<Object, Field> beans = new HashMap<>();
+
+
+    private Optional<String> getDefault(String value) {
+        return Optional.ofNullable(value.contains(":") ? value.substring(value.indexOf(":")+1) : null);
+    }
+
+    private String getConfigFromDB(String name, Optional<String> defValue) {
+        Optional<SysConfigEntity> sysConfig = new SysConfigEntity().setName(name).query().get(dataSource);
+        Assert.isTrue(sysConfig.isPresent() || defValue.isPresent(),"Config value not found: " + name);
+        if (sysConfig.isEmpty()) {
+            sysConfig = Optional.of(new SysConfigEntity().setName(name).setValue(defValue.get()).persist().insert(dataSource));
+        }
+        return sysConfig.get().getValue();
+    }
+
+    @SneakyThrows
+    private Object getValue(Object bean,Field field) {
+        DbValue dbValue = field.getAnnotation(DbValue.class);
+        String value = dbValue.value().replace("${","").replace("}","");
+        String name = value.contains(":") ? value.substring(0,value.indexOf(":")) : value;
+
+        String sysValue = getConfigFromDB(name,getDefault(value));
+
+        return field.getType().getMethod("valueOf", String.class).invoke(bean, sysValue);
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        return bean;
+    }
+
+    public Object postProcessBeforeInitialization(final Object bean, String beanName)
+            throws BeansException {
+        ReflectionUtils.doWithFields(bean.getClass(), field -> {
+            if (field.getAnnotation(DbValue.class) != null) {
+                field.setAccessible(true);
+                refreshBean(bean, field);
+                Scope scope = bean.getClass().getAnnotation(Scope.class);
+                if (scope == null || !scope.value().equals("prototype")) {
+                    beans.put(bean, field);
+                }
+            }
+        });
+        return bean;
+    }
+
+    @SneakyThrows
+    private void refreshBean(Object bean,Field field) {
+        field.set(bean,getValue(bean,field));
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void refreshCachedValues() {
+        beans.keySet().stream().forEach(k -> refreshBean(k,beans.get(k)));
+    }
+}
